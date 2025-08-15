fdf5d54f001e52b713ffb5d62f0b5dbd3921adc6
Mampionona-Rakotovao <mampiononarakotovao06@gmail.com>
2025-08-15 13:42:04 +0300

    feat: first middleware done

--- api_laravel/routes/api.php
+++ api_laravel/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\PhotoController;
+use App\Http\Middleware\PhotoMiddleware;
 
 Route::get('/way', function () {
     return 'hello world!';
@@ -20,5 +21,5 @@ Route::get('/mampy', [TestController::class, 'getMethod']);
 Route::post('/mampy', [TestController::class, 'postMethod']);
 
 //route pour les photos
-Route::post('/photo',[PhotoController::class, 'store']);
+Route::post('/photo',[PhotoController::class, 'store'])->middleware(PhotoMiddleware::class);
 
